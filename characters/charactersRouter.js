3a493719a2bcb8ed1240f8f09f7ed94ca027fc2c
Smithface <kylewibblesmith@gmail.com>
2018-03-15 18:54:38 -0400

    added movies to get char w/ id response

--- characters/charactersRouter.js
+++ characters/charactersRouter.js
@@ -1,25 +1,38 @@
 const express = require('express');
 
 const Character = require('./Character.js');
+const Film = require('../films/Film');
 
 const router = express.Router();
 
 // add endpoints here
 router.get('/:id', function (req,res) {
   const { id } = req.params;
-
+  // const charQuery = Character
   Character
     .findById(id)
     .sort('episode')
     .populate('homeworld')
-    .populate('movies')
     .then(char => {
-      console.log(char.movies);
-      res.status(200).json(char);
-    })
+      const key = char.key;
+      Film.find({})
+        .sort('episode')
+        .select('character_ids title')
+        .then(movies => {
+          return movies.filter(movie => {
+            return movie.character_ids.includes(key)})
+          })
+        .then(foundMovies => {
+          fakeChar = char.toObject();
+          fakeChar.movies = foundMovies
+          res.status(200).json(fakeChar);
+        })
+        .catch(err => console.log(err));
+      })
     .catch(err => {
       res.status(500).json(err);
-    })
+    });
+
 })
 
 module.exports = router;
