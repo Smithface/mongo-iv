ce561cedc9714d9971aabd8774e48ea35c24d95a
Smithface <kylewibblesmith@gmail.com>
2018-03-15 16:42:20 -0400

    added first get by id request to chars

--- characters/charactersRouter.js
+++ characters/charactersRouter.js
@@ -5,5 +5,21 @@ const Character = require('./Character.js');
 const router = express.Router();
 
 // add endpoints here
+router.get('/:id', function (req,res) {
+  const { id } = req.params;
+
+  Character
+    .findById(id)
+    .sort('episode')
+    .populate('homeworld')
+    .populate('movies')
+    .then(char => {
+      console.log(char.movies);
+      res.status(200).json(char);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    })
+})
 
 module.exports = router;
--- films/filmsRouter.js
+++ films/filmsRouter.js
@@ -7,15 +7,19 @@ const router = express.Router();
 // add endpoints here
 router.get('/', function(req, res) {
   const producerFilter = req.query.producer;
+  const releaseFilter = req.query.released;
+
   let query = Film
     .find({})
     .sort('episode')
     // .populate('characters', 'name gender height skin_color')
     // .populate('planets', 'name climate terrain gravity diameter')
-    .select('title producer')
+    .select('title producer release_date created')
 
   if (producerFilter) {
     query.where({ producer: /gary kurtz/i })
+  } else if (releaseFilter) {
+    query.where({ release_date: /2005-*/ })
   }
 
     query.then(films => {
@@ -23,5 +27,11 @@ router.get('/', function(req, res) {
     })
     .catch();
 });
+
+// function toTitleCase(str)
+// {
+//     return str.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+// }
+
 
 module.exports = router;
